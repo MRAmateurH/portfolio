50b1d986a34e43705260bb5e591a896ca9438ab6
EricHsu <hsu14253@gmail.com>
2023-11-14 03:38:21 +0800

    modified: package-lock.json version

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+// react@17
 import ReactDOM from "react-dom";
+// react@18
+// import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// react@17
+ReactDOM.render(
 	<BrowserRouter>
 		<App />
-	</BrowserRouter>
+	</BrowserRouter>,
+	document.getElementById("root")
 );
+
+// react@18
+// ReactDOM.createRoot(document.getElementById("root")).render(
+// 	<BrowserRouter>
+// 		<App />
+// 	</BrowserRouter>
+// );
